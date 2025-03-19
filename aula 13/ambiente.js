d9e0088756a10f22e5ceaadb18787a768ba8aa1c
Carlos1986New <carlosplayergw2@gmail.com>
2025-03-19 20:36:15 -0300

    Praticando laços de repetição

---
+++ aula 13/ambiente.js
@@ -0,0 +1,22 @@
+/* Para estrutura FOR
+for (c = 1; c<= 10; c++){
+    console.log(`Passo ${c}`)
+}
+*/
+
+
+/* Para estrutura DO WHILE
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 15)
+*/
+
+
+/* Para estrutura WHILE
+while (c <= 15){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
