ecec66f5cba715299ec6af80bdeb4c65264698ac
Carlos1986New <carlosplayergw2@gmail.com>
2025-03-23 20:36:24 -0300

    atualização

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,5 +1,4 @@
 let num = [1, 2, 3, 5, 10, 11]
 num[3] = 6
-num.sort()
 console.log(`Nosso vetor é o ${num}`)
 console.log(num.length)
--- aula12ex/Ex014/script.js
+++ aula12ex/Ex014/script.js
@@ -2,7 +2,8 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 6
 
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 6 && hora < 12){
